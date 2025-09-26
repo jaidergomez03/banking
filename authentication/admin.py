1caf91822a6374c150d0cd2239e0510c4a9f0d2b
jaider <jaidergomez20@gmail.com>
2025-09-26 12:50:44 -0500

    entorno django

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import Countries,Departments,Cities,Users # Importando tus modelos
 # Register your models here.
+admin.site.register(Countries)
+admin.site.register(Departments)
+admin.site.register(Cities)
+admin.site.register(Users)
