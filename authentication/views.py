42795e51afcf5fe70b03e2bb4cb885baaba236af
Jaider <jaidergomez20@gmail.com>
2025-10-06 20:49:23 -0500

    taller

--- authentication/views.py
+++ authentication/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Country
 from .forms import CountryForm
+from django .http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("This is my first view")
 
 def country_list(request):
     countries = Country.objects.all().order_by('name')
-    return render(request, 'banking/country_list.html', {'countries': countries})
+    return render(request, "authentication_app/country_list.html", {"countries": countries})
 
 def country_edit(request, pk):
     country = get_object_or_404(Country, pk=pk)
     if request.method == 'POST':
         form = CountryForm(request.POST, instance=country)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'País actualizado correctamente.')
-            return redirect('country_list')
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'País actualizado correctamente.')
+        return redirect('country_list')
     else:
         form = CountryForm(instance=country)
-    return render(request, 'banking/country_form.html', {'form': form, 'country': country})
+        return render(request, "authentication_app/country_list.html", {"countries": country})
